d09054c58519bdf5f1b5be4ba7600160ed0c0dad
KBanause <kaba@kbanause.com>
2012-10-15 01:31:51 +0200

    Added FileVersionInfo

--- mrfunktionen.pas
+++ mrfunktionen.pas
@@ -13,12 +13,6 @@ interface
 uses
   SysUtils,
   StrUtils,
-  {$IFDEF WINDOWS}
-  windows,
-  {$ENDIF}
-  {$IFDEF UNIX}
-
-  {$ENDIF}
   Classes;
 
 const
@@ -30,6 +24,28 @@ type
   TIntArray = array of Integer;
   TDblArray = array of Extended;
   TDblSep = (dsKomma, dsPunkt);
+  TFileVersionInfo = record
+    FileType,
+    CompanyName,
+    FileDescription,
+    FileVersion,
+    InternalName,
+    LegalCopyRight,
+    LegalTradeMarks,
+    OriginalFileName,
+    ProductName,
+    ProductVersion,
+    Comments,
+    SpecialBuildStr,
+    PrivateBuildStr,
+    FileFunction: string;
+    DebugBuild,
+    PreRelease,
+    SpecialBuild,
+    PrivateBuild,
+    Patched,
+    InfoInferred: Boolean;
+  end;
 
 function Between(Value, Min, Max: Integer): Boolean; overload;
 function Between(Value, Min, Max: Integer; excl: Boolean): Boolean; overload;
@@ -37,6 +53,7 @@ function BinaryToHexStr(BinWert: string): string;
 function CreateConfigFile(AppName: string; ConfigFileExtension: string): string;
 function CreateConfigFile(ConfigFileExtension: string): string;
 function CreateConfigFile: string;
+function FileVersionInfo(const sAppNamePath: TFileName; defVersion: String = ''): TFileVersionInfo;
 function GetBaseName(FileName: string): string; overload;
 function GetBaseName: string; overload;
 function GetConfigFileName(AppName: string; ConfigFileExtension: string): string;
@@ -56,26 +73,20 @@ function Implode(const Glue: string; const Pieces: array of Integer): string; ov
 function Implode(const Glue: string; const Pieces: array of string): string; overload;
 function InArray(const Wert: string; arra: TStrArray): Integer;
 function MaxIntArray(const iArray: TIntArray): Int64;
-function MRPosEx(const SubStr: string; const S: string; Offset: Integer;
-  Reverse: Boolean = False): Integer;
+function MRPosEx(const SubStr: string; const S: string; Offset: Integer; Reverse: Boolean = False): Integer;
 function OsterSonntag(Jahr: Integer): TDateTime;
-function ReplaceStr(SubStr: string; Str: string; NewStr: string;
-  RepAll: Boolean = false): string;
-function ReplaceString(SubStr: string; Str: string; NewStr: string;
-  RepAll: Boolean = false): string;
+function ReplaceStr(SubStr: string; Str: string; NewStr: string; RepAll: Boolean = false): string;
+function ReplaceString(SubStr: string; Str: string; NewStr: string; RepAll: Boolean = false): string;
 function ReverseString(Str: string): string;
 function StringToHex(str: string): string;
 function Trenn1000er(Wert: Int64): string;
 
-procedure CopyArray(srcArray: TStrArray; startPosSrc: Integer;
-  var dstArray: TStrArray; startPosDst: Integer; anzahl: Integer);
 procedure Chomp(var Str: string);
-procedure Explode(const Werte, Trenner: string; var ResArray: TDblArray;
-  DblSep: TDblSep; Limit: Integer = 0; const EndeElement: Boolean = false); overload;
-procedure Explode(const Werte, Trenner: string; var ResArray: TIntArray;
-  Limit: Integer = 0; const EndeElement: Boolean = false); overload;
-procedure Explode(const Werte, Trenner: string; var ResArray: TStrArray;
-  Limit: Integer = 0; const EndeElement: Boolean = false); overload;
+procedure CopyArray(srcArray: TStrArray; startPosSrc: Integer; var dstArray: TStrArray; startPosDst: Integer; anzahl: Integer);
+procedure Explode(const Werte, Trenner: string; var ResArray: TDblArray; DblSep: TDblSep; Limit: Integer = 0; const EndeElement: Boolean = false); overload;
+procedure Explode(const Werte, Trenner: string; var ResArray: TIntArray; Limit: Integer = 0; const EndeElement: Boolean = false); overload;
+procedure Explode(const Werte, Trenner: string; var ResArray: TStrArray; Limit: Integer = 0; const EndeElement: Boolean = false); overload;
+procedure GetCurrentScreen(var Index: Integer; var Extends: TRect);
 procedure IntArrayToStrArray(ia: TIntArray; var sa: TStrArray);
 procedure QuickSort(var Ar: TIntArray);
 procedure QuickSort(var Ar: TStrArray);
@@ -85,11 +96,19 @@ procedure Swap(var p1, p2: Extended); overload;
 procedure Swap(var p1, p2: Int64); overload;
 procedure Swap(var p1, p2: Pointer); overload;
 procedure Swap(var p1, p2: string); overload;
-procedure GetCurrentScreen(var Index: Integer; var Extends: TRect);
 
 implementation
 
-uses DateUtils, Forms;
+uses DateUtils,
+  {$IFDEF WINDOWS}
+  windows,
+  ShellApi,
+  {$ENDIF}
+  {$IFDEF UNIX}
+  unix,
+  baseunix,
+  {$ENDIF}
+     Forms;
 
 {Funktionen und Prozeduren}
 
@@ -1004,6 +1023,167 @@ begin
   end; // if (not DirectoryExists(p))
 
   Result := s;
+end;
+
+function FileVersionInfo(const sAppNamePath: TFileName; defVersion: String = ''): TFileVersionInfo;
+{$IFDEF WINDOWS}
+var
+  rSHFI: TSHFileInfo;
+  iRet: Integer;
+  VerSize: Integer;
+  VerBuf: PChar;
+  VerBufValue: Pointer;
+  VerHandle: Cardinal;
+  VerBufLen: Cardinal;
+  VerKey: string;
+  FixedFileInfo: PVSFixedFileInfo;
+
+  // dwFileType, dwFileSubtype
+  function GetFileSubType(FixedFileInfo: PVSFixedFileInfo): string;
+  begin
+    case FixedFileInfo^.dwFileType of
+
+      VFT_UNKNOWN: Result    := 'Unknown';
+      VFT_APP: Result        := 'Application';
+      VFT_DLL: Result        := 'DLL';
+      VFT_STATIC_LIB: Result := 'Static-link Library';
+
+      VFT_DRV:
+        case
+          FixedFileInfo^.dwFileSubtype of
+          VFT2_UNKNOWN: Result         := 'Unknown Driver';
+          VFT2_DRV_COMM: Result        := 'Communications Driver';
+          VFT2_DRV_PRINTER: Result     := 'Printer Driver';
+          VFT2_DRV_KEYBOARD: Result    := 'Keyboard Driver';
+          VFT2_DRV_LANGUAGE: Result    := 'Language Driver';
+          VFT2_DRV_DISPLAY: Result     := 'Display Driver';
+          VFT2_DRV_MOUSE: Result       := 'Mouse Driver';
+          VFT2_DRV_NETWORK: Result     := 'Network Driver';
+          VFT2_DRV_SYSTEM: Result      := 'System Driver';
+          VFT2_DRV_INSTALLABLE: Result := 'InstallableDriver';
+          VFT2_DRV_SOUND: Result       := 'Sound Driver';
+        end;
+      VFT_FONT:
+        case FixedFileInfo^.dwFileSubtype of
+          VFT2_UNKNOWN: Result       := 'Unknown Font';
+          VFT2_FONT_RASTER: Result   := 'Raster Font';
+          VFT2_FONT_VECTOR: Result   := 'Vector Font';
+          VFT2_FONT_TRUETYPE: Result := 'Truetype Font';
+          else;
+        end;
+      VFT_VXD: Result := 'Virtual Defice Identifier = ' +
+          IntToHex(FixedFileInfo^.dwFileSubtype, 8);
+    end;
+  end;
+
+
+  function HasdwFileFlags(FixedFileInfo: PVSFixedFileInfo; Flag: Word): Boolean;
+  begin
+    Result := (FixedFileInfo^.dwFileFlagsMask and
+      FixedFileInfo^.dwFileFlags and
+      Flag) = Flag;
+  end;
+
+  function GetFixedFileInfo: PVSFixedFileInfo;
+  begin
+    if not VerQueryValue(VerBuf, '', Pointer(Result), VerBufLen) then
+      Result := nil
+  end;
+
+  function GetInfo(const aKey: string): string;
+  begin
+    Result := '';
+    VerKey := Format('\StringFileInfo\%.4x%.4x\%s',
+      [LoWord(Integer(VerBufValue^)),
+      HiWord(Integer(VerBufValue^)), aKey]);
+    if VerQueryValue(VerBuf, PChar(VerKey), VerBufValue, VerBufLen) then
+      Result := StrPas(VerBufValue);
+  end;
+
+  function QueryValue(const aValue: string): string;
+  begin
+    Result := '';
+    // obtain version information about the specified file
+    if GetFileVersionInfo(PChar(sAppNamePath), VerHandle, VerSize, VerBuf) and
+      // return selected version information
+      VerQueryValue(VerBuf, '\VarFileInfo\Translation', VerBufValue, VerBufLen) then
+      Result := GetInfo(aValue);
+  end;
+{$ENDIF}
+begin
+  // Initialize the Result
+  with Result do
+  begin
+    FileType         := '';
+    CompanyName      := '';
+    FileDescription  := '';
+    FileVersion      := '';
+    InternalName     := '';
+    LegalCopyRight   := '';
+    LegalTradeMarks  := '';
+    OriginalFileName := '';
+    ProductName      := '';
+    ProductVersion   := '';
+    Comments         := '';
+    SpecialBuildStr  := '';
+    PrivateBuildStr  := '';
+    FileFunction     := '';
+    DebugBuild       := False;
+    Patched          := False;
+    PreRelease       := False;
+    SpecialBuild     := False;
+    PrivateBuild     := False;
+    InfoInferred     := False;
+  end;
+
+  {$IFNDEF WINDOWS}
+  Result.FileVersion := SPVERSION;
+  {$ELSE}
+  // Get the file type
+  if SHGetFileInfo(PChar(sAppNamePath), 0, rSHFI, SizeOf(rSHFI), SHGFI_TYPENAME) <> 0 then
+  begin
+    Result.FileType := rSHFI.szTypeName;
+  end;
+
+  iRet := SHGetFileInfo(PChar(sAppNamePath), 0, rSHFI, SizeOf(rSHFI), SHGFI_EXETYPE);
+  if iRet <> 0 then
+  begin
+    // determine whether the OS can obtain version information
+    VerSize := GetFileVersionInfoSize(PChar(sAppNamePath), VerHandle);
+    if VerSize > 0 then
+    begin
+      VerBuf := AllocMem(VerSize);
+      try
+        with Result do
+        begin
+          CompanyName      := QueryValue('CompanyName');
+          FileDescription  := QueryValue('FileDescription');
+          FileVersion      := QueryValue('FileVersion');
+          InternalName     := QueryValue('InternalName');
+          LegalCopyRight   := QueryValue('LegalCopyRight');
+          LegalTradeMarks  := QueryValue('LegalTradeMarks');
+          OriginalFileName := QueryValue('OriginalFileName');
+          ProductName      := QueryValue('ProductName');
+          ProductVersion   := QueryValue('ProductVersion');
+          Comments         := QueryValue('Comments');
+          SpecialBuildStr  := QueryValue('SpecialBuild');
+          PrivateBuildStr  := QueryValue('PrivateBuild');
+          // Fill the VS_FIXEDFILEINFO structure
+          FixedFileInfo := GetFixedFileInfo;
+          DebugBuild    := HasdwFileFlags(FixedFileInfo, VS_FF_DEBUG);
+          PreRelease    := HasdwFileFlags(FixedFileInfo, VS_FF_PRERELEASE);
+          PrivateBuild  := HasdwFileFlags(FixedFileInfo, VS_FF_PRIVATEBUILD);
+          SpecialBuild  := HasdwFileFlags(FixedFileInfo, VS_FF_SPECIALBUILD);
+          Patched       := HasdwFileFlags(FixedFileInfo, VS_FF_PATCHED);
+          InfoInferred  := HasdwFileFlags(FixedFileInfo, VS_FF_INFOINFERRED);
+          FileFunction  := GetFileSubType(FixedFileInfo);
+        end;
+      finally
+        FreeMem(VerBuf, VerSize);
+      end
+    end;
+  end;
+  {$ENDIF}
 end;
 
 function GetBaseName(FileName: string): string;
